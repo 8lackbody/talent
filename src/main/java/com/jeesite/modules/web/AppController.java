a64cac4a271666dddf50d28e4aa753bf6bd397a8
TheBlack <32874589+8lackbody@users.noreply.github.com>
2021-07-01 15:06:20 +0800

    添加手动更新代码

--- src/main/java/com/jeesite/modules/web/AppController.java
+++ src/main/java/com/jeesite/modules/web/AppController.java
@@ -16,11 +16,13 @@ import com.jeesite.modules.utils.HttpUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -33,7 +35,7 @@ import java.util.stream.Collectors;
  * @author zht
  * @version 2020-12-15
  */
-@Controller
+@RestController
 @RequestMapping(value = "app/")
 public class AppController {
 
@@ -44,6 +46,12 @@ public class AppController {
 
     @Autowired
     ArchivesService archivesService;
+
+    @Value("${App.phoneVersion}")
+    String phoneVersion;
+
+    @Value("${App.tabletVersion}")
+    String tabletVersion;
 
     /**
      * 查询列表
@@ -163,4 +171,94 @@ public class AppController {
         }
     }
 
+    /**
+     * 检查安卓是否需要升级
+     *
+     * @param version 当前使用中的版本号
+     * @param type    设备类型 1手持机 2广告机
+     * @return 返回是否可以升级
+     */
+    @RequestMapping(value = "checkVersion")
+    @ResponseBody
+    public ResultVo checkVersion(@RequestParam("version") String version, @RequestParam("deviceType") Integer type) {
+        File file = null;
+        switch (type) {
+            // 1是手持机，对比版本号，确认是否有升级文件
+            case 1:
+                file = new File(System.getProperty("user.dir") + "\\src\\main\\resources\\static\\phone.apk");
+                if (file.exists() && isNeedUpdate(version, phoneVersion)) {
+                    return ResultVo.ok().put(true);
+                } else {
+                    return ResultVo.ok().put(false);
+                }
+                // 2是广告机，对比版本号，确认是否有升级文件
+            case 2:
+                file = new File(System.getProperty("user.dir") + "\\src\\main\\resources\\static\\tablet.apk");
+                if (file.exists() && isNeedUpdate(version, tabletVersion)) {
+                    return ResultVo.ok().put(true);
+                } else {
+                    return ResultVo.ok().put(false);
+                }
+            default:
+                return ResultVo.ok().put(false);
+        }
+    }
+
+    /**
+     * 对比版本号，看是否需要升级
+     *
+     * @param checkedVersion 被检查的版本号
+     * @param version        服务器中的版本号
+     * @return 是否需要升级
+     */
+    public boolean isNeedUpdate(String checkedVersion, String version) {
+        try {
+            // 这里的对比逻辑是把版本号中的点去掉，然后比数字大小
+            Integer iver1 = Integer.parseInt(checkedVersion.replaceAll("\\.", ""));
+            Integer iver2 = Integer.parseInt(version.replaceAll("\\.", ""));
+            if (iver1 >= iver2) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return false;
+        }
+    }
+
+
+    /**
+     * 下载更新文件
+     *
+     * @param deviceType 设备类型 1手持机 2广告机
+     * @return
+     */
+    @RequestMapping("/download")
+    public ResponseEntity<byte[]> downloadFile(@RequestParam("deviceType") String deviceType) {
+        try {
+            // 根据不同的设备类型获取不同的apk文件
+            String fileUrl;
+            if ("1".equals(deviceType)) {
+                fileUrl = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\phone.apk";
+            } else {
+                fileUrl = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\tablet.apk";
+            }
+            InputStream in = new FileInputStream(fileUrl);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024 * 4];
+            int n = 0;
+            while ((n = in.read(buffer)) != -1) {
+                out.write(buffer, 0, n);
+            }
+            in.close();
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("Content-Type", "application/octet-stream");
+            return new ResponseEntity<>(out.toByteArray(), httpHeaders, HttpStatus.OK);
+        } catch (IOException e) {
+            // 如果异常发送空包
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
